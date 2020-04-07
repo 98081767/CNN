0d6656a1f48cbe9848e847e5513a4f1abbf1d768
98081767 <98081767@student.uts.edu.au>
2020-04-07 18:14:58 +1000

    updated code

--- Flowers.py
+++ Flowers.py
@@ -168,12 +168,25 @@ x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.25,random_state=4
 #set random seeds
 np.random.seed(42)
 rn.seed(42)
-tf.set_random_seed(42)
+tf.random.set_seed(42)
 
 
 #modelling
 
 # # modelling starts using a CNN.
+#NOTES:
+# - Max Pooling is a way to introduce noise by downsampling to prevent overfitting - See https://protect-au.mimecast.com/s/JqZhCOMK2OTAAEOPiv-kbX?domain=computersciencewiki.org
+# - Padding:
+#       VALID Padding: it means no padding and it assumes that all the dimensions are valid so that the input image gets fully covered by a filter and the stride specified by you. and its like saying, we are ready to loose some information. 
+#       SAME Padding: it applies padding to the input image so that the input image gets fully covered by the filter and specified stride.It is called SAME because, for stride 1 , the output will be the same as the input.
+#       See: https://intellipaat.com/community/558/what-is-the-difference-between-same-and-valid-padding-in-tf-nn-maxpool-of-tensorflow
+# - Filter size:
+#       https://www.pyimagesearch.com/2018/12/31/keras-conv2d-and-convolutional-layers/
+#       Notice at as our output spatial volume is decreasing our number of filters learned is increasing — this is a common practice in designing CNN architectures and one I recommend you do as well. As far as choosing the appropriate number of filters
+#       , I nearly always recommend using powers of 2 as the values.
+#       You may need to tune the exact value depending on (1) the complexity of your dataset and (2) the depth of your neural network, but I recommend starting with filters in the range [32, 64, 128] in the earlier and increasing up to [256, 512, 1024] in the deeper layers.
+
+
 
 model = Sequential()
 model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',activation ='relu', input_shape = (150,150,3)))
@@ -194,3 +207,130 @@ model.add(Flatten())
 model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dense(5, activation = "softmax"))
+
+
+#Using a LR Annealer
+# - reduces learning rate when loss stops improving:
+# - https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/ReduceLROnPlateau
+#Note: Need to add this to the model.fit as a call back
+
+batch_size=128
+epochs=50
+
+from keras.callbacks import ReduceLROnPlateau
+red_lr= ReduceLROnPlateau(monitor='val_acc',patience=3,verbose=1,factor=0.1)
+
+
+#data augmentation to prevent overfitting
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(x_train)
+
+
+#compiling and summary
+
+model.compile(optimizer=Adam(lr=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
+model.summary()
+
+#Fitting on the Training set and making predictions on the Validation set
+#using fit_generator() - https://www.pyimagesearch.com/2018/12/24/how-to-use-keras-fit-and-fit_generator-a-hands-on-tutorial/
+
+History = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_test,y_test),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size)
+
+# Evaluating the Model Performance
+
+plt.plot(History.history['loss'])
+plt.plot(History.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+
+plt.plot(History.history['accuracy'])
+plt.plot(History.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'test'])
+plt.show()
+
+#Visualizing Predictons on the Validation Set
+
+# getting predictions on val set.
+pred=model.predict(x_test)
+pred_digits=np.argmax(pred,axis=1)
+
+# now storing some properly as well as misclassified indexes'.
+i=0
+prop_class=[]
+mis_class=[]
+
+for i in range(len(y_test)):
+    if(np.argmax(y_test[i])==pred_digits[i]):
+        prop_class.append(i)
+    if(len(prop_class)==8):
+        break
+
+i=0
+for i in range(len(y_test)):
+    if(not np.argmax(y_test[i])==pred_digits[i]):
+        mis_class.append(i)
+    if(len(mis_class)==8):
+        break
+    
+    
+#CORRECTLY CLASSIFIED FLOWER IMAGES
+
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+
+count=0
+fig,ax=plt.subplots(4,2)
+fig.set_size_inches(15,15)
+
+for i in range (4):
+    for j in range (2):
+        ax[i,j].imshow(x_test[prop_class[count]])
+        #ax[i,j].set_title("Predicted Flower : "+str(le.inverse_transform([pred_digits[prop_class[count]]]))+"\n"+"Actual Flower : "+str(le.inverse_transform(np.argmax([y_test[prop_class[count]]]))))
+        plt.tight_layout()
+        count+=1
+        
+        
+#MISCLASSIFIED IMAGES OF FLOWERS
+
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+
+count=0
+fig,ax=plt.subplots(4,2)
+fig.set_size_inches(15,15)
+for i in range (4):
+    for j in range (2):
+        ax[i,j].imshow(x_test[mis_class[count]])
+        #ax[i,j].set_title("Predicted Flower : "+str(le.inverse_transform([pred_digits[mis_class[count]]]))+"\n"+"Actual Flower : "+str(le.inverse_transform(np.argmax([y_test[mis_class[count]]]))))
+        plt.tight_layout()
+        count+=1
+        
+        
+
+
+
+
+
